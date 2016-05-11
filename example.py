4b40c7924b73288941e6777724ba6dd48fde46cf
AhnSeongHyun <sh84.ahn@gmail.com>
2016-05-11 16:41:20 +0900

    add tests and new example.json.postman_collection

--- example.py
+++ example.py
@@ -2,4 +2,4 @@
 __author__ = 'sh84.ahn@gmail.com'
 
 import postman2md
-postman2md.convert(postman_file="example.json.postman_collection", multi_file=False)
+postman2md.convert(postman_file='postman2md_example.json.postman_collection', multi_file=False)
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,33 +1,36 @@
 # -*- coding:utf-8 -*-
+import os 
 import unittest
 import requests 
 
 class BasicTestCase(unittest.TestCase):
     
     def setUp(self):
-        postman_official_json_collection = 'https://schema.getpostman.com/json/collection/v1/'
-        r = requests.get(postman_official_json_collection)
-        self.dump_file_path= './collection.json'
-        if r.status_code == 200:
-            self.collection_json = r.text 
-            with open(self.dump_file_path, 'w') as f:
-                f.write(r.text)
-        else:
-            self.assertFalse()
-
+        self.directory = 'postman2md_example'
+        self.collection_file_path = 'postman2md_example.json.postman_collection'
 
     def test_single(self):
         import postman2md
-        postman2md.convert(postman_file=self.dump_file_path, multi_file=False)
-        
+        postman2md.convert(postman_file=self.collection_file_path, multi_file=False)
 
+        file_count =  len([name for name in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, name))])
+        self.assertEqual(file_count, 1)
+            
     def test_multi(self):
+       
         import postman2md
-        postman2md.convert(postman_file=self.dump_file_path)
+        postman2md.convert(postman_file=self.collection_file_path)
+        file_count =  len([name for name in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, name))])
+        self.assertGreater(file_count, 1)
 
+
+
+ 
 
     def tearDown(self):
-        import os 
-        if os.path.exists(self.dump_file_path):
-            os.remove(self.dump_file_path)
+        if os.path.exists(self.directory):
+            import shutil
+            shutil.rmtree(self.directory)
+                             
+
 
